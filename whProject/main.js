dd2189becc69a75afa2e5f382bbfbe95cf028928
kjacobsen327 <123105389+kjacobsen327@users.noreply.github.com>
2023-01-28 23:12:09 -0600

    Add files via upload

--- whProject/main.js
+++ whProject/main.js
@@ -1,22 +1,3 @@
-import ScoreboardView from "./scoreboard/ScoreboardView.js";
-
-let playerOneScore = 0;
-let playerTwoScore = 0;
-const root = document.querySelector("#app");
-const view = new ScoreboardView(root, "Command Points", "Victory Points", (player, direction) => {
-	const difference = direction === "minus" ? -1 : 1;
-
-	if (player === "one") {
-		playerOneScore = Math.max(playerOneScore + difference, 0);
-	} else {
-		playerTwoScore = Math.max(playerTwoScore + difference, 0);
-	}
-
-	view.update(playerOneScore, playerTwoScore);
-});
-
-
-
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -31,3 +12,21 @@ for (i = 0; i < coll.length; i++) {
     } 
     });
 }
+
+function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
+  
+  function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
