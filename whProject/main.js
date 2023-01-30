162b4cdcdeec68441548709850d4d2a597ba2c1b
kjacobsen327 <123105389+kjacobsen327@users.noreply.github.com>
2023-01-29 21:40:15 -0600

    Add files via upload

--- whProject/main.js
+++ whProject/main.js
@@ -1,3 +1,51 @@
+var Command_Points = 0;
+var Command_PointsElement = document.getElementById("Command_Points");
+var Command_PointsStorage = localStorage.getItem("Command_Points");
+var Victory_Points = 0;
+var Victory_PointsElement = document.getElementById("Victory_Points");
+var Victory_PointsStorage = localStorage.getItem("Victory_Points");
+
+if (Command_PointsStorage) {
+  Command_Points = parseInt(Command_PointsStorage);
+  Command_PointsElement.innerHTML = Command_Points;
+}
+if (Victory_PointsStorage) {
+  Victory_Points = parseInt(Victory_PointsStorage);
+  Victory_PointsElement.innerHTML = Victory_Points;
+}
+
+function subtractCommand_Points() {
+  Command_Points--;
+  Command_PointsElement.innerHTML = Command_Points;
+  localStorage.setItem("Command_Points", Command_Points);
+}
+function addCommand_Points() {
+  Command_Points++;
+  Command_PointsElement.innerHTML = Command_Points;
+  localStorage.setItem("Command_Points", Command_Points);
+}
+function subtractVictory_Points() {
+  Victory_Points--;
+  Victory_PointsElement.innerHTML = Victory_Points;
+  localStorage.setItem("Victory_Points", Victory_Points);
+}
+function addVictory_Points() {
+  Victory_Points++;
+  Victory_PointsElement.innerHTML = Victory_Points;
+  localStorage.setItem("Victory_Points", Victory_Points);
+}
+
+
+function resetScore() {
+  Command_Points = 0;
+  Command_PointsElement.innerHTML = Command_Points;
+  localStorage.setItem("Command_Points", Command_Points);
+  Victory_Points = 0;
+  Victory_PointsElement.innerHTML = Victory_Points;
+  localStorage.setItem("Victory_Points", Victory_Points);
+}
+
+//Expanding Button Script//
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -13,6 +61,7 @@ for (i = 0; i < coll.length; i++) {
     });
 }
 
+//Battleplan Buttons Sript//
 function openTab(tabName) {
     var i, x;
     x = document.getElementsByClassName("containerTab");
