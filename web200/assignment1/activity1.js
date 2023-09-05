82be81f09e9ff1ff3c5c7c8f1a988a575318480b
Kevin Jacobsen <k_jacobsen12@mail.harpercollege.edu>
2023-09-04 19:38:01 -0500

    finished assignment 1 activity 1

---
+++ web200/assignment1/activity1.js
@@ -0,0 +1,9 @@
+document.write("Hello, Kevin!!!");
+let greeting = "Hello, ";
+let myName = "Kevin!";
+document.write("<p>");
+document.write(greeting + myName);
+document.write("</p>");
+window.alert("Hello, Kevin!");
+document.getElementById("myName").innerHTML = "Helloooo, Kevin!!!!!!!!!!"
+console.log("Hello! My name is Kevin!");
