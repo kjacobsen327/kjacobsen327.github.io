1fcaf94243d9d4268f2584bb5f1fe683f441c986
Kevin Jacobsen <k_jacobsen12@mail.harpercollege.edu>
2023-09-04 20:48:06 -0500

    Assignment 1 updated

--- web200/assignment1/activity1.js
+++ web200/assignment1/activity1.js
@@ -1,9 +1,14 @@
-document.write("Hello, Kevin!");
+window.alert("The first way to generate output: Hello, my name is Kevin!");
+
+
+document.write("The second way: Hello, Kevin!");
+
 let greeting = "Hello, ";
 let myName = "Kevin!!!";
 document.write("<p>");
-document.write(greeting + myName);
+document.write("A third way: " + greeting + myName);
 document.write("</p>");
-window.alert("Hello, Kevin!");
-document.getElementById("myName").innerHTML = "Helloooo, Kevin!!!!!!!!!!"
+
+document.getElementById("myName").innerText = "This is actually the fourth way: I am changing the text of the <p> element";
+
 console.log("Hello! My name is Kevin!");
