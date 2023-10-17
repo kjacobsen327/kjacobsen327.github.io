bfffa7889b6bb20a1834077230bf343aa2c9799e
Kevin Jacobsen <k_jacobsen12@mail.harpercollege.edu>
2023-10-16 20:05:25 -0500

    fix socksize

--- web200/assignment5/activity3.js
+++ web200/assignment5/activity3.js
@@ -1,19 +1,21 @@
+let sockSize = document.getElementById("sockSize");
+
 document.getElementById("shoeSize").addEventListener("click", (e) => {
     switch (e.target.getAttribute("id")) {
         case "xs":
-            document.getElementById("sockSize").innerHTML ="extra small";
+            sockSize.innerHTML ="extra small";
             break;
         case "s":
-            document.getElementById("sockSize").innerHTML = "small";
+            sockSize.innerHTML = "small";
             break;
         case "m":
-            document.getElementById("sockSize").innerHTML = "medium";
+            sockSize.innerHTML = "medium";
             break;
         case "l":
-            document.getElementById("sockSize").innerHTML = "large";
+            sockSize.innerHTML = "large";
             break;
         case "xl":
-            document.getElementById("sockSize").innerHTML = "extra large";
+            sockSize.innerHTML = "extra large";
             break;
 
     }
