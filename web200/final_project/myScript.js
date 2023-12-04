97a1f5746630c90afb184c2bdbf697383b451db4
Kevin Jacobsen <k_jacobsen12@mail.harpercollege.edu>
2023-12-04 14:52:18 -0600

    display updated toppings cost

--- web200/final_project/myScript.js
+++ web200/final_project/myScript.js
@@ -1,16 +1,18 @@
 let selectedCrustSize = "";
+let iPrice = 0;
 let selectedToppings = [];
 let pizza = {};
 
 let crustSizeOptions = document.querySelectorAll("input[name=crust-size]");
 let toppingOptions = document.querySelectorAll("input[name=toppings]");
-
+let toppingsCost = document.getElementById("toppingsCost");
 
 for (let i = 0; i < crustSizeOptions.length; i++) {
     crustSizeOptions[i].addEventListener("click", function () {
         selectedCrustSize = this.value;
         pizza.size = selectedCrustSize;
         console.log(pizza);
+        updateToppingsCost(selectedCrustSize);
     })
 }
 
@@ -18,14 +20,27 @@ for (let i = 0; i < toppingOptions.length; i++) {
     toppingOptions[i].addEventListener("click", function () {
         if (this.checked) {
             selectedToppings.push(this.value);
-            console.log(selectedToppings);
         }
         else if (this.checked == false) {
             let index = selectedToppings.indexOf(this.value);
             selectedToppings.splice(index, 1);
-            console.log(selectedToppings);
         }
         pizza.toppings = selectedToppings;
         console.log(pizza);
     })
+}
+
+function updateToppingsCost(crustSize) {
+    switch (crustSize) {
+        case "small":
+            iPrice = 2;
+            break;
+        case "medium":
+            iPrice = 2.5;
+            break;
+        case "large":
+            iPrice = 3;
+            break;
+    }
+    toppingsCost.innerHTML = "$" + iPrice.toFixed(2) + " each";
 }
