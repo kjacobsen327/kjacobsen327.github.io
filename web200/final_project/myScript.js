f61a91d7559435bcad1d1288fb96f049d63a86bf
Kevin Jacobsen <k_jacobsen12@mail.harpercollege.edu>
2023-12-04 18:36:17 -0600

    subtotal working

--- web200/final_project/myScript.js
+++ web200/final_project/myScript.js
@@ -1,22 +1,50 @@
-let selectedCrustSize = "";
-let iPrice = 0;
-let selectedToppings = [];
-let pizza = {};
 
+let customer = {};
+let pizza = {};
+let pizzasBuilt = [];
+let order = {
+    pizzasBuilt: [],
+    customer: {}
+};
+let pizzaCounter = 1; //to keep track of and name pizzas in order
+
+let pizzaOptions = document.getElementById("pizzaOptions").getElementsByTagName("input");
 let crustSizeOptions = document.querySelectorAll("input[name=crust-size]");
 let toppingOptions = document.querySelectorAll("input[name=toppings]");
 let toppingsCost = document.getElementById("toppingsCost");
+let priceText = document.getElementById("price");
+let addPizzaBtn = document.getElementById("addPizza");
+
+addPizzaBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+});
+addPizzaBtn.addEventListener("click", addPizzaToOrder);
+
+// window.addEventListener("load", createNewPizza);
+// function createNewPizza() {
+let selectedCrustSize = "";
+let iPrice = 0;
+let selectedToppings = [];
+let price = 0;
 
 for (let i = 0; i < crustSizeOptions.length; i++) {
+    if (crustSizeOptions[i].checked == false) {
+        for (let i = 0; i < toppingOptions.length; i++) {
+            toppingOptions[i].disabled = true;
+        }
+    }
     crustSizeOptions[i].addEventListener("click", function () {
+        for (let i = 0; i < toppingOptions.length; i++) {
+            toppingOptions[i].disabled = false;
+        }
         selectedCrustSize = this.value;
-        pizza.size = selectedCrustSize;
-        console.log(pizza);
+        // pizza.size = selectedCrustSize;
         updateToppingsCost(selectedCrustSize);
     })
 }
 
 for (let i = 0; i < toppingOptions.length; i++) {
+
     toppingOptions[i].addEventListener("click", function () {
         if (this.checked) {
             selectedToppings.push(this.value);
@@ -25,8 +53,6 @@ for (let i = 0; i < toppingOptions.length; i++) {
             let index = selectedToppings.indexOf(this.value);
             selectedToppings.splice(index, 1);
         }
-        pizza.toppings = selectedToppings;
-        console.log(pizza);
     })
 }
 
@@ -43,4 +69,53 @@ function updateToppingsCost(crustSize) {
             break;
     }
     toppingsCost.innerHTML = "$" + iPrice.toFixed(2) + " each";
+}
+
+for (let i = 0; i < pizzaOptions.length; i++) {
+    pizzaOptions[i].addEventListener("click", updatePrice);
+}
+
+function updatePrice() {
+    switch (selectedCrustSize) {
+        case "small":
+            price = 14;
+            break;
+        case "medium":
+            price = 17;
+            break;
+        case "large":
+            price = 20;
+            break;
+    }
+    price = price + selectedToppings.length * iPrice;
+    priceText.innerHTML = "$" + price.toFixed(2);
+    // pizza.price = price;
+}
+
+function Pizza(selectedCrustSize, selectedToppings, price) {
+    this.size = selectedCrustSize,
+        this.toppings = selectedToppings,
+        this.price = price;
+}
+function addPizzaToOrder() {
+    let pizza = new Pizza(selectedCrustSize, selectedToppings, price);
+    pizza.name = "Pizza " + pizzaCounter;
+    pizzaCounter++;
+    pizzasBuilt.push(pizza);
+    console.log(selectedToppings);
+    console.log(pizza);
+    console.log(pizzasBuilt);
+    updateSubtotal();
+}
+
+let orderDisplay = document.getElementById("ordersList");
+
+function updateSubtotal() {
+    let subtotal = 0;
+
+    for (let i = 0; i < pizzasBuilt.length; i++) {
+
+        subtotal += pizzasBuilt[i].price;
+    }
+    orderDisplay.innerHTML = subtotal;
 }
