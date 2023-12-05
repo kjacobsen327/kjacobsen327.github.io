70027b238fa43738703b9e972e37565fafc69ba9
Kevin Jacobsen <k_jacobsen12@mail.harpercollege.edu>
2023-12-05 17:12:05 -0600

    added remove, submit btns and customerinfo

---
+++ web200/final_project/script.js
@@ -0,0 +1,149 @@
+let orderList = [];
+let ingredientPrices = {
+    pepperoni: { small: 1.5, medium: 2, large: 2.5 },
+    mushrooms: { small: 1, medium: 1.5, large: 2 },
+    onions: { small: 0.5, medium: 1, large: 1.5 },
+    sausage: { small: 2, medium: 2.5, large: 3 },
+    Peppers: { small: 1, medium: 1.5, large: 2 },
+    olives: { small: 1, medium: 1.5, large: 2 }
+};
+let crustPrices = {
+    small: 5,
+    medium: 8,
+    large: 12
+};
+
+let taxRate = 0.08;
+
+function addToOrder() {
+    let crustSize = document.querySelector('input[name="crustSize"]:checked').value;
+    let selectedIngredients = [];
+    let ingredientCheckboxes = document.querySelectorAll('input[name="ingredients"]:checked');
+    
+    for (let i = 0; i < ingredientCheckboxes.length; i++) {
+        selectedIngredients.push(ingredientCheckboxes[i].value);
+    }
+    let pizza = {
+        crustSize: crustSize,
+        ingredients: selectedIngredients
+    };
+
+    // let customer = {
+    //     name: document.getElementById('name').value,
+    //     address: document.getElementById('address').value,
+    //     city: document.getElementById('city').value,
+    //     state: document.getElementById('state').value,
+    //     zip: document.getElementById('zip').value,
+    //     phone: document.getElementById('phone').value,
+    //     email: document.getElementById('email').value
+    // };
+
+    // Calculate total price for the pizza
+    let totalPrice = calculateTotalPrice(pizza);
+
+    let order = {
+        pizza: pizza,
+        // customer: customer,
+        totalPrice: totalPrice
+    };
+
+    orderList.push(order);
+    displayOrderSummary();
+    updateOrderTotals();
+}
+
+
+
+function calculateTotalPrice(pizza) {
+    let crustPrice = crustPrices[pizza.crustSize];
+    let ingredientPrice = 0;
+
+    pizza.ingredients.forEach(function (ingredient) {
+        ingredientPrice += ingredientPrices[ingredient][pizza.crustSize];
+    });
+
+    return crustPrice + ingredientPrice;
+}
+
+function displayOrderSummary() {
+    let summaryList = document.getElementById('orderSummary');
+    summaryList.innerHTML = "";
+
+    // Get each order from orderList array
+    orderList.forEach(function (order, index) {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = `
+            Pizza ${index + 1}:<br>
+            Crust Size: ${order.pizza.crustSize}<br>
+            Ingredients: ${order.pizza.ingredients.join(', ')}<br>
+
+            Total Price: $${order.totalPrice.toFixed(2)}
+            <button class="removeButton" onclick="removePizza(${index})">Remove</button>
+        `;
+        summaryList.appendChild(listItem);
+    });
+}
+
+function removePizza(index) {
+    orderList.splice(index, 1);
+    displayOrderSummary();
+    updateOrderTotals();
+}
+
+function updateOrderTotals() {
+    let subtotal = orderList.reduce(function (acc, order) {
+        return acc + order.totalPrice;
+    }, 0);
+
+    let tax = subtotal * taxRate;
+    let grandTotal = subtotal + tax;
+
+    document.getElementById('subtotal').textContent = `Subtotal: $${subtotal.toFixed(2)}`;
+    document.getElementById('tax').textContent = `Tax (8%): $${tax.toFixed(2)}`;
+    document.getElementById('grandTotal').textContent = `Grand Total: $${grandTotal.toFixed(2)}`;
+}
+
+function submitOrder() {
+    // Check if customer information is complete
+    if (
+        document.getElementById('name').value === '' ||
+        document.getElementById('address').value === '' ||
+        document.getElementById('city').value === '' ||
+        document.getElementById('state').value === '' ||
+        document.getElementById('zip').value === '' ||
+        document.getElementById('phone').value === '' ||
+        document.getElementById('email').value === ''
+    ) {
+        alert('Please complete all customer information before submitting the order.');
+        return;
+    }
+
+    // Create a complete order object
+    let completeOrder = {
+        
+        customerInfo: {
+            name: document.getElementById('name').value,
+            address: document.getElementById('address').value,
+            city: document.getElementById('city').value,
+            state: document.getElementById('state').value,
+            zip: document.getElementById('zip').value,
+            phone: document.getElementById('phone').value,
+            email: document.getElementById('email').value
+        },
+        pizzas: orderList,
+        orderTotals: {
+            subtotal: parseFloat(document.getElementById('subtotal').textContent.replace('Subtotal: $', '')),
+            tax: parseFloat(document.getElementById('tax').textContent.replace('Tax (8%): $', '')),
+            grandTotal: parseFloat(document.getElementById('grandTotal').textContent.replace('Grand Total: $', ''))
+        }
+    };
+
+    // Log the complete order object to the console
+    console.log(completeOrder);
+
+    // Reset the form and order list
+    document.getElementById('pizzaForm').reset();
+    orderList = [];
+    displayOrderSummary();
+    updateOrderTotals();
+}
